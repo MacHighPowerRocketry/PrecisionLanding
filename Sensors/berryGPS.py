2fbe065ac0d3ac13f69fcc4a8b8943970560490e
emilybthorpe <ethorpe@macalester.edu>
2023-02-20 14:06:03 -0600

    reworked gps, fixing error causing no output

--- Sensors/berryGPS.py
+++ Sensors/berryGPS.py
@@ -1,78 +1,46 @@
 from Sensors.sensor import sensor
-from BerryIMU import IMU
 import time
 import smbus
 import signal
-import sys
 BUS = None
 address = 0x42
 gpsReadInterval = 0.03
 
+
 class berryGPS(sensor):
 
     def __init__(self, logger):
-        sensor.__init__(self,logger)
+        sensor.__init__(self, logger)
         self.logger = logger
         signal.signal(signal.SIGINT, self.handle_ctrl_c)
         self.connectBus()
+        self.gpsString = None
 
     def connectBus(self):
         self.bus = smbus.SMBus(1)
 
-    def checkForCharError(self, gpsLine):
-        CharError = 0
-
-        for c in gpsLine:                               # Check #3, Make sure that only readiable ASCII charaters and Carriage Return are seen.
-            if (c < 32 or c > 122) and  c != 13:
-                CharError+=1
-
-        return CharError == 0
-
-    def checkGPSLine(self, gpsLine):
-        """
-        basic sanity check on gps line
-        make sure '$' doesnt appear twice and 83 is maximun NMEA sentenace length
-        """
-        return gpsLine.count(36) == 1 and len(gpsLine) < 84
-        
-    def checkForAllocationError(self, gpsChars):
-        return gpsChars.find('txbuf') == -1
-
-    def manualChecksum(self, gpsStr, chkSum):
-        # Remove the $ and do a manual checksum on the rest of the NMEA sentence
-         # Compare the calculated checksum with the one in the NMEA sentence
-        for ch in gpsStr[1:]: 
-            chkVal ^= ord(ch)
-        return chkVal != int(chkSum, 16)
-
-
-    
     def parseResponse(self, gpsLine):
-        if(not self.checkGPSLine):
-            return
-
-        if(not self.checkForCharError()):
-            return
-        
-        gpsChars = ''.join(chr(c) for c in gpsLine)
-        if(not self.checkForAllocationError(gpsChars)):
-            return
-
-        gpsStr, chkSum = gpsChars.split('*',2)  # Check #5 only split twice to avoid unpack error
-        gpsComponents = gpsStr.split(',')
-        if(not self.manualChecksum(gpsStr, chkSum)):
-            return
-            
-        return gpsChars
-
-    def handle_ctrl_c(self, signal, frame):
-        sys.exit(130)
+        if (gpsLine.count(36) == 1):
+            if len(gpsLine) < 84:
+                CharError = 0
+                for c in gpsLine:
+                    if (c < 32 or c > 122) and c != 13:
+                        CharError += 1
+                if (CharError == 0):
+                    gpsChars = ''.join(chr(c) for c in gpsLine)
+                    if (gpsChars.find('txbuf') == -1):
+                        gpsStr, chkSum = gpsChars.split('*', 2)
+                        chkVal = 0
+                        for ch in gpsStr[1:]:
+                            chkVal ^= ord(ch)
+                        if (chkVal == int(chkSum, 16)):
+                            self.gpsString = gpsChars
 
     def readGPS(self):
         c = None
         response = []
         try:
-            while True: # Newline, or bad char.
+            while True:
                 c = BUS.read_byte(address)
                 if c == 255:
                     return False
@@ -80,17 +48,16 @@ class berryGPS(sensor):
                     break
                 else:
                     response.append(c)
-            return self.parseResponse(response)
+            self.parseResponse(response)
         except IOError:
-            self.logger.debugLog("io error")
             self.connectBus()
         except Exception as err:
             self.logger.debugLog(err)
-        return None 
 
     def toString(self):
-        return self.readGPS()
-        
+        self.readGPS()
+        return self.gpsString
+
 
 if __name__ == '__main__':
     import rocketLogger
